acc09de6bdbef9d73ce52352e4644041dca4313e
Nahidul Hasan <nahidul.cse@gmail.com>
2018-08-09 13:36:02 +0600

    Added PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -13,12 +13,9 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Post::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph
     ];
 });
-
